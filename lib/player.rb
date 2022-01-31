b06e34fa88197294e405ca259a09a9779e191190
WilfriedPaillot <paillotwilfried@gmail.com>
2022-02-01 00:54:48 +0100

    Added get_damages method

--- lib/player.rb
+++ lib/player.rb
@@ -9,5 +9,14 @@ class Player
   def show_state
     puts "#{name} a #{life_points} points de vie"
   end
+
+  def gets_damages(damages)
+    if damages.respond_to?(:to_i)
+      self.life_points -= damages
+      if @life_points <= 0
+        puts "Le joueur #{name} a été tué"
+      end
+    end
+  end
   
 end
