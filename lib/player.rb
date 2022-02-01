ddd50b87389f3dca5b1c787a32bfed6015be82c3
WilfriedPaillot <paillotwilfried@gmail.com>
2022-02-01 01:31:51 +0100

    Updated variables & adde attacks method w/ rand

--- lib/player.rb
+++ lib/player.rb
@@ -7,16 +7,27 @@ class Player
   end
 
   def show_state
-    puts "#{name} a #{life_points} points de vie"
+    puts "#{@name} a #{@life_points} points de vie"
   end
 
   def gets_damages(damages)
-    if damages.respond_to?(:to_i)
-      self.life_points -= damages
+    #if damages.respond_to?(:to_i)
+      @life_points -= damages
       if @life_points <= 0
-        puts "Le joueur #{name} a été tué"
+        puts "Le joueur #{@name} a été tué"
       end
-    end
+    #end
+  end
+
+  def attacks(victim)
+    puts "Le joueur #{@name} attaque le joueur #{victim.name}"
+    damages = compute_damages
+    victim.gets_damages(damages)
+    puts "#{@name} vient d'infliger #{damages} points de dégats"
+  end
+
+  def compute_damages
+    return rand(1..6)
   end
   
 end
